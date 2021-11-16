863bf9a40023b90e8ba1c1ecf3cc7f82a1d98195
a100538 <a100538@alunos.uminho.pt>
2021-11-16 09:55:48 +0000

    adicinada documentacao T1 e apagadas funcoes nao utilizadas

--- src/Tarefa1_2021li1g025.hs
+++ src/Tarefa1_2021li1g025.hs
@@ -9,9 +9,68 @@ Módulo para a realização da Tarefa 1 do projeto de LI1 em 2021/22.
 module Tarefa1_2021li1g025 where
 
 import LI12122
+-- * Função principal
+{- | Testa todos os critérios, recebendo o mapa sob o nome "pecas".
+ 
+ ====Para tal, é necessário armazenar estes valores:
 
+ * Ordenar o mapa
+
+     > mapa = ordena pecas
+
+ * Obter o maior valor de coluna e linha (x,y)
+
+     @
+     xmax = xMax mapa
+     ymax = yMax mapa
+     @
+
+ * Criar uma lista das colunas
+
+     > colunas = getColunas mapa xmax
+
+ * Obter a "base" do mapa (últimos blocos de cada coluna)
+
+     > base = getBase colunas
+
+ * Obter o número de cada peça (exceto do Vazio)
+
+     > (portas,blocos,caixas) = contaPecas mapa
+
+ ====Com isto, podemos testar todos os critérios
+
+ Se algum destes for __True__, então o mapa __não__ é válido:
+
+ * Não haver mais que uma declaração de peça para a mesma posição
+
+     > not (pecaSingular mapa) 
+
+ * Declarar exatamente uma porta
+
+     > portas /= 1
+
+ * Todas as caixas devem estar posicionadas em cima de outra caixa ou bloco
+
+     > not (validaCaixas mapa)
+
+ * Deve existir pelo menos um expaço vazio 
+
+     Número total de blocos /= número de casas totais no mapa
+
+     > (xmax+1)*(ymax+1) == (portas+blocos+caixas)
+
+ * A base do mapa deve ser composta por blocos
+
+     Não podem haver colunas sem blocos
+
+     > elem [] colunas
+
+     Os blocos da base têm de ser "válidos"
+
+     > not (validaBase base colunas)
+-}
 validaPotencialMapa :: [(Peca, Coordenadas)] -> Bool
-validaPotencialMapa pecas | length colunas /= xmax + 1 = False
+validaPotencialMapa pecas | elem [] colunas = False
                           | not (pecaSingular mapa) = False
                           | not (validaBase base colunas) = False 
                           | portas /= 1 = False
@@ -25,23 +84,26 @@ validaPotencialMapa pecas | length colunas /= xmax + 1 = False
                                 ymax = yMax mapa
                                 (portas,blocos,caixas) = contaPecas mapa
 
--- ordena a lista de pontos segundo x e y crescente
+-- * Funções auxiliares
+
+-- | Ordena o mapa segundo x e y crescente
 ordena :: [(Peca,Coordenadas)] -> [(Peca,Coordenadas)]
 ordena [] = []
 ordena (x:l) = inserePeca x (ordena l)
 
+-- | Insere uma peça numa lista (ordenada) de peças
 inserePeca :: (Peca, Coordenadas) -> [(Peca,Coordenadas)] -> [(Peca,Coordenadas)]
 inserePeca x [] = [x]
 inserePeca p1@(_,(x1,y1)) (p2@(_,(x2,y2)):l) | x1 > x2 = p2:(inserePeca p1 l)
                                              | y1 > y2 = p2:(inserePeca p1 l)
                                              | otherwise = p1:p2:l-- assume-se que nunca pode ser igual a uma peca existente
 
--- devolve a altura maxima
+-- | Devolve o y maximo do mapa
 yMax :: [(Peca,Coordenadas)] -> Int
 yMax [] = 0
 yMax l = case (last l) of (_,(_,y)) -> y
 
--- devolve o comprimento maximo, sendo dados os ultimos elementos de cada linha
+-- | Devolve o x maximo do mapa
 xMax :: [(Peca,Coordenadas)] -> Int
 xMax [] = 0
 xMax [(_,(x,_))] = x
@@ -49,13 +111,7 @@ xMax ((p1@(_,(x,_))):l) | x > x2 = x
                         | otherwise = x2
                         where x2 = xMax l
 
--- devolve uma linha, dada a sua altura (yMax para ultima linha)
-getLinha :: [(Peca,Coordenadas)] -> Int -> [(Peca,Coordenadas)]
-getLinha [] _ = []
-getLinha ((e@(_,(_,y))):l) n | y == n = e:(getLinha l n)
-                             | otherwise = getLinha l n
-
--- conta o numero de portas, caixas e blocos
+-- | Conta o numero de portas, caixas e blocos
 contaPecas :: [(Peca,Coordenadas)] -> (Int,Int,Int)
 contaPecas [] = (0,0,0)
 contaPecas ((x,_):l) | x == Porta = (1+a,b,c)
@@ -64,15 +120,16 @@ contaPecas ((x,_):l) | x == Porta = (1+a,b,c)
                      | otherwise = (a,b,c)
                      where (a,b,c) = contaPecas l
 
--- devolve pecas dada as coordenadas ( devolve lista para verificar repetencias ou nao existencia de pecas)
+-- | Devolve pecas dada as suas coordenadas. Devolve uma lista para que possamos verificar repetições ou não existência de peças
 getPeca :: [(Peca,Coordenadas)] -> Coordenadas -> [(Peca,Coordenadas)]
 getPeca [] _ = []
 getPeca ((p,c1):l) c2 | c1 == c2 = (p,c1):(getPeca l c2)
                       | otherwise = getPeca l c2
 
---devolve coluna DE BLOCOS e o mapa sem essa coluna (assim quando uma coluna está calculada podemos ignora la)
--- funciona
-getColuna :: [(Peca,Coordenadas)] -> Int -> ([(Peca,Coordenadas)],[(Peca,Coordenadas)])
+-- | Devolve uma coluna de blocos e o mapa sem essa coluna, para maior eficiência na função 'getColunas'
+getColuna :: [(Peca,Coordenadas)] -- ^ Mapa
+ -> Int -- ^ Índice da coluna (x)
+  -> ([(Peca,Coordenadas)],[(Peca,Coordenadas)]) -- ^ (coluna,mapa sem coluna)
 getColuna [] _ = ([],[])
 getColuna (p@(peca,(x,_)):l) n | peca == Bloco = if x == n
                                                  then (p:a,b)
@@ -80,20 +137,26 @@ getColuna (p@(peca,(x,_)):l) n | peca == Bloco = if x == n
                                | otherwise = (a,b)
                                where (a,b) = getColuna l n
 
--- devolve uma lista de listas em que cada lista são as pecas de uma coluna (n = xMax do mapa)
--- cuidado com a ordem das colunas
-getColunas :: [(Peca,Coordenadas)] -> Int -> [[(Peca,Coordenadas)]] 
+-- | Devolve uma lista de listas em que cada lista corresponde a uma coluna
+
+-- | As colunas ficam ordenadas segundo y crescente, mas segundo x decrescente
+getColunas :: [(Peca,Coordenadas)] -- ^ Mapa
+ -> Int -- ^ Maior índice de coluna (xmax)
+  -> [[(Peca,Coordenadas)]] -- ^ Lista de colunas
 getColunas l n | n < 0 = []
                | otherwise = a:(getColunas b (n-1))
                where (a,b) = getColuna l n
 
--- devolve blocos mais "baixos" de cada coluna
+-- | Devolve blocos mais "baixos" de cada coluna
 getBase :: [[(Peca,Coordenadas)]] -> [(Peca,Coordenadas)]
 getBase [] = []
 getBase (x:l) | x == [] = getBase l
               | otherwise = (last x):(getBase l)
 
+{- | Verifica se uma dada peça está "suportada" por outra peça numa coluna adjacente.
 
+Para isso, sendo dada essa peça e a coluna à sua esquerda, testa se existem peças adjacentes à peça
+-}
 validaPeca :: (Peca,Coordenadas) -> [(Peca,Coordenadas)] -> Bool
 validaPeca _ [] = False
 validaPeca (peca,(x,y)) l | getPeca l (x-1,y-1) /= [] = True
@@ -101,7 +164,36 @@ validaPeca (peca,(x,y)) l | getPeca l (x-1,y-1) /= [] = True
                           | getPeca l (x-1,y+1) /= [] = True
                           | otherwise = False
 
-validaBase :: [(Peca,Coordenadas)] -> [[(Peca,Coordenadas)]] -> Bool
+{- | Verifica se a base do mapa é válida
+Estes esquemas ilustram como a função valida uma base:
+
+Primeiro, é dada a base do mapa como argumento
+
+<<map1.png>> <<map2.png>> <<map3.png>>
+
+<<map1.2.png>> <<map2.2.png>> <<map3.2.png>>
+
+Depois, da direita para a esqueda, cada peça da base é validada, através da 'validaPeca' que recebe essa peca e a coluna à sua esquerda
+
+<<map1.3.png>> <<map2.3.png>> <<map3.3.png>>
+
+No caso do primeiro mapa, a primeira peça não é válida.
+
+Quando isto acontece, é verificado se existe uma peça acima
+
+<<map1.4.png>>
+
+Se não existir, a base não é válida.
+
+Neste caso, como existe, esta passa a ser a peça que tem de ser validada
+
+<<map1.5.png>>
+
+Este processo é repetido recursivamente até percorrer toda a base ou chegar a um caso não válido.
+-}
+validaBase :: [(Peca,Coordenadas)] -- ^ Base do mapa
+ -> [[(Peca,Coordenadas)]]-- ^ Lista de colunas do mapa
+  -> Bool -- ^ True se a base for válida
 validaBase [_] _ = True
 validaBase (p:base) ct@(c1:c2:colunas) | validaPeca p c2 = continua
                                        | peca /= [] = if validaPeca (head peca) c2
@@ -113,18 +205,7 @@ validaBase (p:base) ct@(c1:c2:colunas) | validaPeca p c2 = continua
                                              peca = getPeca c1 (x,y-1)
 validaBase _ _ = False 
 
--- let mapa = [(Bloco,(0,0)),(Bloco,(0,2)),(Bloco,(1,0)),(Bloco,(2,2))]
--- let mapa2 = [(Bloco,(0,2)),(Bloco,(1,2)),(Bloco,(2,1))] (tem de dar certo)
--- let mapa3 = [(Bloco,(0,2)),(Bloco,(1,2)),(Bloco,(1,1)),(Bloco,(0,2))] (tem de dar certo)
--- let mapa4 = [(Caixa,(0,2)),(Bloco,(0,3)),(Bloco,(1,1)),(Bloco,(1,2)),(Bloco,(2,2)),(Porta,(3,2)),(Bloco,(3,3))]
-{-
-x1 = ordena mapa2
-x2 = xMax x1
-x3 = getColunas x1 x2
-x4 = getBase x3
-validaBase x4 x3
--}
--- 2.1.1 - Função que testa se existem declarações de peças no mesmo síto -- dois processos para exeplorar
+-- | Função que testa se existem declarações de peças no mesmo síto -- dois processos para exeplorar
 -- assume-se que a lista não é vazia
 pecaSingular :: [(Peca, Coordenadas)] -> Bool
 pecaSingular [x] = True
@@ -135,30 +216,7 @@ converSecond :: [(Peca, Coordenadas)] -> [(Coordenadas)]
 converSecond [] = []
 converSecond ((_,(x,y)):t) = (x,y) : (converSecond t)
 
--- TO DO -- Outra forma de resolução da 2.1.1 por explorar
-{-
-pecaSingular :: [(Peca, Coordenadas)] -> Bool
-pecaSingular [x] = True
-pecaSingular l@(h:t) = length (getPeca l h) <= 1 && pecaSingular t
--}
-
---acho que esta se pode apagar
-{- -- 2.1.2 - Função que declara exatamente uma porta
-
-declarePorta :: [(Peca,Coordenadas)] -> Bool
-declarePorta l = contaPortas l == 1
-
- -- TO DO -- 2.1.3 - Função que verifica se as caixas estão em posições permitidas 
- 
- posCaixa :: [(Peca, Coordenadas)] -> Bool
- posCaixa [] = True
- posCaixa l1@((obj1, (x,y)):t) | obj1 == Caixa && obj2 == Caixa = posCaixa ((obj2, cords):t)
-                               | obj1 == Caixa && obj2 /= Bloco = False -- caso a caixa esteja posicionada em cima de uma porta
-                               | obj1 == Caixa = length l1' == 1
-                               | otherwise = posCaixa t
-            where l1'@[(obj2, cords)] = getPeca l1 (x,y-1)
--}
-
+-- | Verifica se todas as caixas do mapa têm um bloco ou uma caixa na posição diretamente abaixo
 validaCaixas :: [(Peca,Coordenadas)] -> Bool
 validaCaixas [] = True
 validaCaixas ((peca,(x,y)):l) | peca == Caixa = (case getPeca l (x,y+1) of [] -> False
