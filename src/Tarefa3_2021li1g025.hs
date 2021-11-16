fcbf28b86e4ff221a90f373883e7e81116be2d37
a100538 <a100538@alunos.uminho.pt>
2021-11-16 14:45:07 +0000

    Completada Tarefa3, mas documentacao incompleta

--- src/Tarefa3_2021li1g025.hs
+++ src/Tarefa3_2021li1g025.hs
@@ -10,5 +10,64 @@ module Tarefa3_2021li1g025 where
 
 import LI12122
 
+-- * Expressão principal
+
+{- | Esta expressão será usada para transformar um jogo em texto, transformando-o numa instância da classe Show.
+
+Para tal, utilizamos as funções 'insereJogador' e 'jogoToText'
+
+> show (Jogo mapa jogador) = insereJogador jogador (jogoToText mapa)
+-}
+
+-- como é que fazemos documentação para a instância???
 instance Show Jogo where
-  show = undefined
+  show (Jogo mapa jogador) = insereJogador jogador (jogoToText mapa)
+
+-- * Funções utilizadas
+
+-- | Aplica 'jogoToTextAux' a cada linha, recursivamente, adicionando ao fim da linha "\n".
+
+-- | Quando o mapa tiver apenas uma lista, não adiciona "\n" para que este não fique no final da expressão
+jogoToText :: Mapa -- ^ Lista de listas em que cada lista é uma linha de peças
+ -> [String] -- ^ Igual ao mapa, mas as peças estão transformadas nos seus correspondentes textuais
+jogoToText [] = []
+jogoToText [linha] = [jogoToTextAux linha]
+jogoToText (linha:l) = ((jogoToTextAux linha)++"\n"):(jogoToText l)
+
+{- | Transforma todos os elementos de uma linha nos seus correspondentes textuais
+
+@case x of Bloco -> 'X'
+           Porta -> 'P'
+           Vazio -> ' '
+           Caixa -> 'C'
+@
+-}
+jogoToTextAux :: [Peca] -> String
+jogoToTextAux [] = []
+jogoToTextAux (x:linha) =
+      (case x of Bloco -> 'X'
+                 Porta -> 'P'
+                 Vazio -> ' '
+                 Caixa -> 'C'):(jogoToTextAux linha)
+
+{- | Insere o jogador na lista de Strings criada pelo jogoToText
+
+Dependendo da sua orientação, este corresponderá a '>' ou '<'
+
+A função funciona dividindo a lista de strings segundo o valor de y, obtendo assim a linha que queremos e as linhas antes e após
+
+De seguida, a string obtida é dividida segundo o valor de x, permitindo obter a 'casa' que queremos, bem como as casas antes e após
+
+Se o jogador puder ocupar essa casa, ela será substituída pela sua representação e todas as Strings resultantes são concatenadas numa só 
+-}
+-- é suposto representar a caixa em cima do jogador??
+insereJogador :: Jogador -> [String] -> String
+insereJogador _ [] = error "posição inválida"
+insereJogador (Jogador (x,y) dir caixa) l | posicao == ' ' = (concat l1) ++ ((a ++ [(case dir of Oeste -> '<'
+                                                                                                 Este -> '>')]) ++ b) ++ (concat l2)
+                                          | otherwise = error "posição ocupada"
+                                          where (l1,(linha:l2)) = splitAt y l
+                                                (a,posicao:b) = splitAt x linha
+
+
+-- incompleto l22 l63
