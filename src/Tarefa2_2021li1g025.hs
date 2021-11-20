064a0f43ed297d7acd987218935f686269714b1e
a100709 <a100709@alunos.uminho.pt>
2021-11-20 23:58:57 +0000

    Terminada a função desconstroiMapa e terminada a Tarefa2

--- src/Tarefa2_2021li1g025.hs
+++ src/Tarefa2_2021li1g025.hs
@@ -10,6 +10,7 @@ module Tarefa2_2021li1g025 where
 
 import LI12122
 
+
 -- devolve o indice maximo de linha numa lista
 yMax :: [(Peca,Coordenadas)] -> Int
 yMax [] = 0
@@ -28,11 +29,14 @@ getPeca [] _ = []
 getPeca ((p,c1):l) c2 | c1 == c2 = (p,c1):(getPeca l c2)
                       | otherwise = getPeca l c2
 
--- devolve uma linha de elementos, dado o seu índice de linha
+-- devolve uma linha de elementos, dado o índice da linha pretendido
 getLinha :: [(Peca,Coordenadas)] -> Int -> [(Peca,Coordenadas)]
 getLinha [] _ = []
 getLinha ((h@(_,(_,y))):t) linha | y == linha = h:(getLinha t linha)
                                  | otherwise = getLinha t linha
+
+
+
 
 -- função que devolve um mapa (esquema) de peças nas suas posições respetivas, incluindo espaços vazios
 -- supõe-se que a lista já está organizada
@@ -59,14 +63,16 @@ constroiLinhaAux l@(h:t) coluna linha cnst | coluna == cnst = [(fst h)] -- quand
                                            | otherwise = (fst h) : constroiLinhaAux t (coluna+1) linha cnst
 
 
-{-
+
+
+
+
 -- Outras funções auxiliares aplicadas a listas de peças (SEM COORDENADAS) para "DescontroiMapa"
 
 -- devolve o indice maximo de linha numa lista
 yMax2 :: [[Peca]] -> Int
-yMax2 [] = 0
-yMax2 (h:t) | length h > yMax2 t = length h
-            | otherwise = yMax2 t
+yMax2 [x] = 0     -- a linha 0 também existe
+yMax2 (h:t) = 1 + yMax2 t
 
 -- devolve uma linha de elementos, dado o seu índice de linha
 getLinha2 :: [[Peca]] -> Int -> [Peca]
@@ -75,10 +81,15 @@ getLinha2 l linha = (l !! linha)
 
 
 
--- TODO -- função inversa de "constroiMapa" que transforma uma lista de peças numa lista com as peças que não "Vazio", juntamente com as suas respetivas coordenadas
+-- função inversa de "constroiMapa" que transforma uma lista de peças numa lista com as peças que não "Vazio", juntamente com as suas respetivas coordenadas
 desconstroiMapa :: Mapa -> [(Peca, Coordenadas)]
 desconstroiMapa [] = []
-desconstroiMapa l = [ concat (talvez??) (desconstroiLinha l y) | y <- [0..(yMax2 l)]]
+desconstroiMapa l = agrupaLinhas l 0
+
+-- função que agrupa várias linhas já desconstruídas numa lista global
+agrupaLinhas :: Mapa -> Int -> [(Peca, Coordenadas)]
+agrupaLinhas l cnst | cnst == yMax2 l = desconstroiLinha l cnst
+                    | otherwise = desconstroiLinha l cnst ++ agrupaLinhas l (cnst+1)
 
 -- função principal que constroi uma linha de peças. Antes de passar pela função auxiliar, restringe a lista com que a Aux trabalha a apenas uma lista com os elementos da linha
 desconstroiLinha :: [[Peca]] -> Int -> [(Peca, Coordenadas)]
@@ -90,13 +101,4 @@ desconstroiLinhaAux [] _ _ = []
 desconstroiLinhaAux  (h:t) coluna linha  | h == Vazio = desconstroiLinhaAux t (coluna+1) linha
                                          | otherwise = (h,(coluna,linha)): (desconstroiLinhaAux t (coluna+1) linha)
 
--}
-
-{-
--- IDEIA: ORDENAR MAPA ANTES DE TIRAR LINHAS
--- ordena a lista de pontos segundo x e y crescente
-ordena :: [(Peca,Coordenadas)] -> [(Peca,Coordenadas)]
-ordena [] = []
-ordena (x:l) = inserePeca x (ordena l)
--}
 
